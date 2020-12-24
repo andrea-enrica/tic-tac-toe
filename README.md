676acc71215e188879ff45df23710bdb6abacf9c
andrea-enrica <enricaiordache@gmail.com>
2020-12-24 17:05:48 +0100

    Add css and media, update js and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tic-tac-toe
+This is a project to learn how to use factory functions, scope and closure and module pattern. Also it is a tic-tac-toe game where you can play with another player. 
+
+TODO : Create an AI 
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,29 +3,26 @@ const playerFactory = (name, mark) => {
     return {name, mark};
 }
 
-//Game Board Module
+//Board Game Module
 const gameBoard = (() => {
-    let firstPlayer;
-    let secondPlayer;
     let boardMatrix;
+
+    //Create game grid 
     document.getElementById('board-game').style.setProperty('--grid-rows', 3);
     document.getElementById('board-game').style.setProperty('--grid-columns', 3);
     for (var i=0; i<9; i++) {
         boardMatrix = document.createElement('div');
         boardMatrix.innerText = " ";
-        boardMatrix.setAttribute('class', "empty");
+        boardMatrix.setAttribute('class', "empty" + " " + i);
         document.getElementById('board-game').appendChild(boardMatrix).setAttribute('id', 'board-item');
     }
 
-    //TO-DO
-    //Get player's name
-    document.getElementById('submit').addEventListener('submit', ()=> {
-        console.log(document.getElementById("player-1").value);
-        firstPlayer = document.getElementById("player-1").value;
-        secondPlayer = document.getElementById("player-2").value;
+    //Open popUp form
+    document.getElementsByClassName('new-game')[0].addEventListener('click', function() {
+        document.getElementById("popUpName").style.display = "block";
     });
 
-    //Add board click event listener and chek for winners
+    //Board items click event listener and check for winners
     document.querySelectorAll('#board-item').forEach(item => {
         item.addEventListener('click', () => {
             twoPlayersGame.playRound(item);
@@ -35,44 +32,63 @@ const gameBoard = (() => {
     //Restart button
     document.getElementsByClassName('restart')[0].addEventListener('click', () => {
         twoPlayersGame.clearBoardGame();
-        var nodes = document.getElementById('board-game').childNodes;
-        for(i=0; i<nodes.length; i++) {
-           nodes[i].innerText = " ";
-           nodes[i].setAttribute('class', 'empty');
-        }
     });
 
-    return{boardMatrix, firstPlayer, secondPlayer};
+    return{boardMatrix};
 })();
 
-//Game Module functions
+//Functions game module
 const game = (() => {
-
     //declare players
-    const player1= playerFactory(gameBoard.firstPlayer, "X");
-    const player2= playerFactory(gameBoard.secondPlayer, "O");
-    console.log(player1);
-    
-    //initial points
+    let player1 = playerFactory("", "X");
+    let player2 = playerFactory("", "O");
+
+    //initial state
     let playerTurn = player1;
     let countBoardItem = 0;
+    let winnerBolean = false;
 
+    //submit botton event listener
+    document.getElementById('submit').addEventListener('click', ()=> {
+        clearBoardGame();
+        getPlayerName();
+        document.getElementById("popUpName").style.display = "none";
+        document.getElementById("left-title").style.display = "block";
+        document.getElementById("right-title").style.display = "block";
+        document.getElementById('player-turn').innerText = "";
+    });
+
+    //get player's name
+    function getPlayerName() {
+        player1.name= document.getElementById("player-1").value;
+        player2.name= document.getElementById("player-2").value;
+        document.getElementById('left-name').innerText = document.getElementById("player-1").value;
+        document.getElementById('right-name').innerText = document.getElementById("player-2").value;
+        document.getElementById("player-1").value = "";
+        document.getElementById("player-2").value = "";
+        return {player1, player2}
+    }
+    
     //fill the squares of the board game with curent value of players and check for winner
     function playRound(boardItem){
-        if (playerTurn == player1 && boardItem.innerText == ""){
+        if (playerTurn == player1 && playerTurn.name !== "" && boardItem.innerText == "" && winnerBolean === false){
             boardItem.innerText = `${player1.mark}`;
             boardItem.className = `${player1.mark}`;
             countBoardItem++;
             document.getElementById('player-turn').innerText = "It's " + `${player2.name}'s` + " turn!";
             checkWinner();
             playerTurn = player2;
-        } else if(playerTurn == player2 && boardItem.innerText == "") {
+        } else if(playerTurn == player2 && playerTurn.name !== "" && boardItem.innerText == "" && winnerBolean === false) {
             boardItem.innerText = `${player2.mark}`;
             boardItem.className = `${player2.mark}`;
             countBoardItem++;
             document.getElementById('player-turn').innerText = "It's " + `${player1.name}'s` + " turn!";
             checkWinner();
             playerTurn = player1;
+        } else if (winnerBolean == true) {
+            document.getElementById('player-turn').innerText = "The round is over, please restart the game!";
+        } else {
+            document.getElementById('player-turn').innerText = "You must enter the player names!";
         }
         return countBoardItem;
     }
@@ -91,35 +107,41 @@ const game = (() => {
 
     //winning condition and function
     function checkWinner() {
-        const winningCondition = [
-            [0,1,2], 
-            [3,4,5], 
-            [6,7,8], 
-            [0,3,6], 
-            [1,4,7], 
-            [2,5,8], 
-            [0,4,8], 
-            [2,4,6]
-        ];
         let boardContentArray = checkBoardContent();
-
+        const winningCondition = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+       
         winningCondition.forEach((item, index) => {
             //check winning condition
             if (countBoardItem<9) {
                 if(
                     boardContentArray[item[0]] === playerTurn.mark && 
                     boardContentArray[item[1]] === playerTurn.mark &&
-                    boardContentArray[item[2]] === playerTurn.mark
+                    boardContentArray[item[2]] === playerTurn.mark 
                 ){
-                    document.getElementById('player-turn').innerText = `${playerTurn.name}` + " wins!";
+                    document.getElementById('player-turn').innerText = "Congratulation " + `${playerTurn.name}` + ", you won!";
+                    item.forEach((gridTransformIndex) =>{
+                        var gridItems = document.getElementById('board-game').childNodes;
+                        for(var i = 0; i < gridItems.length; i++) {
+                            gridItems[gridTransformIndex].setAttribute('class', `${playerTurn.mark}` + ' playing');
+                        }
+                    });
+                    winnerBolean = true;
                 }
+            //check for tie
             } else {
                 if(
                     boardContentArray[item[0]] === playerTurn.mark && 
                     boardContentArray[item[1]] === playerTurn.mark &&
-                    boardContentArray[item[2]] === playerTurn.mark
+                    boardContentArray[item[2]] === playerTurn.mark 
                 ){
-                    document.getElementById('player-turn').innerText = `${playerTurn.name}` + " wins!";
+                    document.getElementById('player-turn').innerText = "Congratulation " + `${playerTurn.name}` + ", you won!";
+                    item.forEach((gridTransformIndex) =>{
+                        var gridItems = document.getElementById('board-game').childNodes;
+                        for(var i = 0; i < gridItems.length; i++) {
+                            gridItems[gridTransformIndex].setAttribute('class', `${playerTurn.mark}` + ' playing');
+                        }
+                    });
+                    winnerBolean = true;
                 } else {
                     document.getElementById('player-turn').innerText = "It's tie!";
                 }
@@ -131,8 +153,16 @@ const game = (() => {
     function clearBoardGame() {
         countBoardItem=0;
         checkBoardContent.boardContent = [];
-        document.getElementById('player-turn').innerText = " ";
+        winnerBolean = false;
         playerTurn = player1;
+        document.getElementById('player-turn').innerText = " ";
+        
+        //get every item from grid  and put initial state
+        var gridItems = document.getElementById('board-game').childNodes;
+        for(var i=0; i<gridItems.length; i++) {
+            gridItems[i].innerText = " ";
+            gridItems[i].setAttribute('class', 'empty');
+        }
     }
 
     return {playerTurn, countBoardItem, playRound, clearBoardGame};
